136ca8ae5d69aa764d46c312e71f9338aabf1734
AlejandraCP <alejandracp710@gmail.com>
2018-12-04 09:45:49 -0500

    'get-data-from-form'

--- src/components/AddDate.js
+++ src/components/AddDate.js
@@ -4,11 +4,19 @@ class AddDate extends Component {
 
     state = {};
 
+    namePet = React.createRef();
+    owner =React.createRef();
+    date = React.createRef();
+    hour = React.createRef();
+    symptom = React.createRef();
+
     createDate = e => {
         e.preventDefault();
         this.props.createDate();
+        console.log(this.namePet.current.value);
+        
     }
-    
+
     render() {
         
       return (
@@ -20,32 +28,32 @@ class AddDate extends Component {
                     <div className="form-group row">
                         <label className="col-sm-4 col-lg-2 col-form-label">Nombre Mascota</label>
                         <div className="col-sm-8 col-lg-10">
-                            <input type="text" className="form-control" placeholder="Nombre Mascota" />
+                            <input type="text" className="form-control" placeholder="Nombre Mascota" ref={this.namePet} />
                         </div>
                     </div>
                     <div className="form-group row">
                         <label className="col-sm-4 col-lg-2 col-form-label">Nombre Dueño</label>
                         <div className="col-sm-8 col-lg-10">
-                            <input type="text" className="form-control"  placeholder="Nombre Dueño de la Mascota" />
+                            <input type="text" className="form-control"  placeholder="Nombre Dueño de la Mascota" ref={this.owner} />
                         </div>
                     </div>
 
                     <div className="form-group row">
                         <label className="col-sm-4 col-lg-2 col-form-label">Fecha</label>
                         <div className="col-sm-8 col-lg-4  mb-4 mb-lg-0">
-                            <input type="date" className="form-control" />
+                            <input type="date" className="form-control" ref={this.date}/>
                         </div>                            
 
                         <label className="col-sm-4 col-lg-2 col-form-label">Hora</label>
                         <div className="col-sm-8 col-lg-4">
-                            <input type="time" className="form-control" />
+                            <input type="time" className="form-control" ref={this.hour}/>
                         </div>
                     </div>
 
                     <div className="form-group row">
                         <label className="col-sm-4 col-lg-2 col-form-label">Sintomas</label>
                         <div className="col-sm-8 col-lg-10">
-                            <textarea  className="form-control"></textarea>
+                            <textarea  className="form-control" ref={this.symptom}></textarea>
                         </div>
                     </div>
                     <div className="form-group row justify-content-end">

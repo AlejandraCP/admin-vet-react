96ecad97821f267dc0765ad301986b6d166c3bc0
AlejandraCP <alejandracp710@gmail.com>
2018-12-06 17:41:56 -0500

    'refact'

--- src/components/Cita.js
+++ src/components/Cita.js
@@ -2,17 +2,17 @@ import React, { Component } from 'react';
 
 class Cita extends Component {
     render() { 
-        const date = this.props.info.date,
-            hour = this.props.info.hour
+        const {date, hour, namePet, owner, symptom} = this.props.info;
+
         return ( 
             <div>
                 <h4>Nombre de mascota</h4>
-                <span></span>
-                <p><span>Nombre del dueño:</span></p>
-                <p><span>Fecha:</span>{date}</p>
-                <p><span>Hora:</span></p>
+                <h6>{namePet}</h6>
+                <p><span>Nombre del dueño:</span> {owner}</p>
+                <p><span>Fecha:</span> {date}</p>
+                <p><span>Hora:</span> {hour}</p>
                 <p>Síntomas:</p>
-                <span></span>
+                <p>{symptom}</p>
 
             </div>
          );
